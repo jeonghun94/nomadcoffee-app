52064a653d02ae8d7dc0fec23fc2054331d7cd1a
Jeong Hun <66016555+jeonghun94@users.noreply.github.com>
2022-04-20 19:50:29 +0900

    section 16 ing

--- apollo.js
+++ apollo.js
@@ -1,22 +1,41 @@
-import { ApolloClient, InMemoryCache, makeVar } from "@apollo/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  makeVar,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const isLoggedInVar = makeVar(false);
 export const tokenVar = makeVar("");
 
 export const logUserIn = async (token) => {
-  await AsyncStorage.multiSet([
-    ["token", token],
-    ["loggedIn", "Yes"],
-  ]);
+  await AsyncStorage.setItem("token", token);
   isLoggedInVar(true);
   tokenVar(token);
 };
 
+export const logUserOut = async () => {
+  await AsyncStorage.removeItem(TOKEN);
+  isLoggedInVar(false);
+  tokenVar(null);
+};
+
+const httpLink = createHttpLink({
+  uri: "http://localhost:4000/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      token: tokenVar(),
+    },
+  };
+});
+
 export const client = new ApolloClient({
-  uri:
-    process.env.NODE_ENV === "production"
-      ? "https://instaclone-backend-jh.herokuapp.com/graphql"
-      : "http://localhost:4000/graphql",
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
